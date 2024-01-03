a52918f4f059f18d951b96822b4cae4ea0d8e770
Armin Rest <arest@stsci.edu>
2024-01-03 18:58:01 -0500

    summary table

--- calc_distortions_list.py
+++ calc_distortions_list.py
@@ -6,8 +6,8 @@ Created on Tue Dec 19 10:11:00 2023
 @author: arest
 """
 
-import sys,argparse
-from pdastro import unique,AandB
+import sys,argparse,os
+from pdastro import unique,AandB,pdastroclass
 from calc_distortions import calc_distortions_class
 
 class calc_distortions_list_class(calc_distortions_class):
@@ -15,12 +15,38 @@ class calc_distortions_list_class(calc_distortions_class):
         calc_distortions_class.__init__(self)
         
         self.dict_aper_filt_pupil = {}
+        
+        self.summary = pdastroclass()
+        
+    def remove_summary_entry(self,apername,filtname,pupilname):
+        if len(self.summary.t)>0:
+            ixs = self.summary.ix_equal('apername', apername)
+            ixs = self.summary.ix_equal('filter', filtname,indices=ixs)
+            ixs = self.summary.ix_equal('pupil', pupilname,indices=ixs)
+            if len(ixs)>1:
+                self.summary.write(indices=ixs)
+                raise RuntimeError(f'more than 1 entry for {apername} {filtname} {pupilname}!')
+            elif len(ixs)==1:
+                if self.verbose: print(f'Removing previous entry in summary table for  {apername} {filtname} {pupilname}')
+                self.summary.t.drop(index=ixs,inplace=True)
+        return(0)
 
     def fit_all_distortions(self, apertures=None, filters=None, pupils=None, 
                             outrootdir=None, outsubdir=None,
                             outbasename=None,
+                            summaryfilename=None,
                             ixs_im=None, progIDs=None,
                             raiseErrorflag=True):
+
+        if (summaryfilename is not None) and self.save_coeffs:
+            if os.path.dirname(summaryfilename)=='':
+                self.set_outbasename(outrootdir=outrootdir,outsubdir=outsubdir)
+                summaryfilename = f'{os.path.dirname(self.outbasename)}/{summaryfilename}'    
+            if os.path.isfile(summaryfilename):
+                print(f'Loading summary file {summaryfilename} ...')
+                self.summary.load(summaryfilename)
+            #self.summary.write()
+            #sys.exit(0)
         
         # get the image indices
         ixs_im = self.imtable.getindices(ixs_im)
@@ -63,16 +89,34 @@ class calc_distortions_list_class(calc_distortions_class):
                     continue
                 
                 for pupilname in pupils4filter:
+                    
+                    print('VVVVVVV')
+                    self.summary.write()
+                    self.remove_summary_entry(apername,filtname,pupilname)
+                    print('VVVVVVV')
+                    self.summary.write()
+                    
                     ixs_pupil = self.imtable.ix_equal('pupil', pupilname, indices=ixs_filter)
                     if self.verbose: print(f'###\n### {apername} {filtname} {pupilname}\n###')
                     self.imtable.write(indices=ixs_pupil)
                     
-                    errorflag = self.fit_distortions(apername, filtname, pupilname,
-                                                     outrootdir=outrootdir, outsubdir=outsubdir,
-                                                     outbasename=outbasename,
-                                                     ixs_im=ixs_pupil, progIDs=progIDs)
+                    (errorflag,coefffilename) = self.fit_distortions(apername, filtname, pupilname,
+                                                                     outrootdir=outrootdir, outsubdir=outsubdir,
+                                                                     outbasename=outbasename,
+                                                                     ixs_im=ixs_pupil, progIDs=progIDs)
                     
-                    
+                    ix_result = self.summary.newrow({'apername':apername,'filter':filtname,'pupil':pupilname,
+                                                     'Nim':len(self.ixs_im),
+                                                    'errorflag':errorflag,'coefffilename':os.path.basename(coefffilename)})
+                    cols=['dx_mean','dx_mean_err','dx_stdev','dy_mean','dy_mean_err','dy_stdev','Ngood','Nclip']
+                    self.summary.t.loc[ix_result,cols] = self.Sci2Idl_residualstats.t.loc[0,cols]
+
+        ixs_summary_sorted = self.summary.ix_sort_by_cols(['apername','filter','pupil']) 
+        self.summary.write(indices=ixs_summary_sorted)
+        # Only save into summary files if coefficients were saved as well.
+        if (summaryfilename is not None) and self.save_coeffs:
+            print(f'Saving summary into {summaryfilename}')
+            self.summary.write(summaryfilename,indices=ixs_summary_sorted) 
                 
     
 
@@ -86,10 +130,12 @@ if __name__ == '__main__':
     parser.add_argument('--filters', default=None, nargs='+', type=str, help='list of filter names, e.g. f200w.')
     parser.add_argument('--pupils', default=None, nargs='+', type=str, help='list of pupil names, e.g. clear. ')
     parser.add_argument('--savecoeff', default=False, action='store_true', help='Save the coefficients. If not overridden with --coefffilename, the default name is {apername}_{filtername}_{pupilname}.polycoeff.txt')
+    parser.add_argument('--summaryfilename', default='distortion_summary.txt', help='filename that contains a summary of the distortion fits. If filename has not path, it is saved in the output directory (default=%(default)s).')
     #parser.add_argument('--input_dir', type=str, default=None, help='input_dir is the directory in which the input images are located located (default=%(default)s)')
     #parser.add_argument('--outrootdir', default='.', help='output root directory. The output directoy is the output root directory + the outsubdir if not None. (default=%(default)s)')
     #parser.add_argument('--outsubdir', default=None, help='outsubdir added to output root directory (default=%(default)s)')
     #parser.add_argument('--progIDs', type=int, default=None, nargs="+", help='list of progIDs (default=%(default)s)')
+
 
     parser = distortions.define_options(parser=parser)
 
@@ -107,5 +153,6 @@ if __name__ == '__main__':
     
     distortions.fit_all_distortions(apertures=args.apertures, filters=args.filters, pupils=args.pupils,
                                     outrootdir=args.outrootdir, outsubdir=args.outsubdir,
-                                    outbasename=args.outbasename)
+                                    outbasename=args.outbasename,
+                                    summaryfilename=args.summaryfilename)
         
